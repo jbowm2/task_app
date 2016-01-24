bf291b8ab66fc65cfc9ae54f117b0dc543b7ef03
vagrant <vagrant@homestead>
2016-01-24 05:15:06 +0000

    task app started

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<title>Task Application</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet" type="text/css">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+	</head>
+
+	<body>
+
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Navbar Content -->
+			</nav>
+		</div>
+
+		@yield('content')
+
+	</body>
+
+</html>
